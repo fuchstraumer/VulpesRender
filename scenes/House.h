8d9f7c15fd147286c0e24694692b1087a5fabb17
Keegan <jonesk74@uw.edu>
2017-07-21 17:26:23 -0700

    Re-add Assimp

---
+++ scenes/House.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef VULPES_HOUSE_SCENE_H
+#define VULPES_HOUSE_SCENE_H
+
+#include "stdafx.h"
+#include "BaseScene.h"
+
+namespace house_scene {
+
+	using namespace vulpes;
+
+	class HouseScene : public BaseScene {
+	public:
+
+	};
+
+}
+
+#endif // !VULPES_HOUSE_SCENE_H
