d7aa4d717fd9bd33a064af75aa1a0342a7f49ef6
kjones <kjones@tethers.com>
2017-07-27 09:51:35 -0700

    Add RenderObject stub files: will be abstract class encapsulating and specifying all elements required to create a renderable object.

---
+++ include/render/RenderObject.h
@@ -0,0 +1,12 @@
+#ifndef VULPES_RENDER_OBJECT_H
+#define VULPES_RENDER_OBJECT_H
+#include "vpr_stdafx.h"
+#include "../ForwardDecl.h"
+
+namespace vulpes {
+
+
+
+}
+
+#endif // !VULPES_RENDER_OBJECT_H
