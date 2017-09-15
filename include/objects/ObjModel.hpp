709522779de362cfadb8f727f380df3085381a7c
Keegan Jones <kjones@tethers.com>
2017-09-15 14:43:04 -0700

    Finish implementation of class for .obj models.

--- include/objects/ObjModel.hpp
+++ include/objects/ObjModel.hpp
@@ -11,7 +11,7 @@
 #include "resource/PipelineCache.hpp"
 #include "render/GraphicsPipeline.hpp"
 #include "tinyobj/tiny_obj_loader.h"
-#include "Materials.hpp"
+#include "Material.hpp"
 
 namespace vulpes {
 
@@ -22,7 +22,7 @@ namespace vulpes {
         glm::vec3 cameraPosition;
     };
 
-    class ObjModel {
+    class ObjModel : public TriangleMesh {
         ObjModel(const ObjModel&) = delete;
         ObjModel& operator=(const ObjModel&) = delete;
     public:
@@ -30,23 +30,21 @@ namespace vulpes {
         ObjModel(const Device* dvc);
         ~ObjModel();
 
-        void LoadModelFromFile(const std::string& obj_model_filename);
+        void LoadModelFromFile(const std::string& obj_model_filename, TransferPool* transfer_pool);
+        void CreateShaders(const std::string& vertex_shader_path, const std::string& fragment_shader_path);
+        void CompleteSetup(const glm::mat4& projection, const VkRenderPass& renderpass);
         void UpdateUBO(const glm::mat4& view);
 
     private:
 
-        void createMaterials(const std::vector<tinyobj::material_t>& materials);
-        void createBuffers();
-        void createTexture();
-        void createResources(TransferPool* transfer_pool);
-        void uploadResources(TransferPool* transfer_pool);
-
-        void createShaders();
+        void loadMeshes(const std::vector<tinyobj::shape_t>& shapes, const tinyobj::attrib_t& attrib, TransferPool* transfer_pool);
+        void createMaterials(const std::vector<tinyobj::material_t>& materials, TransferPool* transfer_pool);
         void createPipelineCache();
-        void createDescriptorSet(DescriptorPool* descriptor_pool);
         void createPipelineLayout();
         void setPipelineStateInfo();
-        void createGraphicsPipeline();
+        void createGraphicsPipeline(const VkRenderPass& renderpass);
+
+        DescriptorPool* descriptorPool;
 
         constexpr static VkVertexInputBindingDescription bindDescription {
             0, sizeof(vertex_t), VK_VERTEX_INPUT_RATE_VERTEX
@@ -58,14 +56,19 @@ namespace vulpes {
             VkVertexInputAttributeDescription{ 2, 0, VK_FORMAT_R32G32_SFLOAT, 2 * sizeof(glm::vec3) }
         };
 
-        std::vector<std::unique_ptr<Material>> materials;
+        // Pools materials so multiple models won't create duplicate material objects, as these are quite costly (tons of texture data)
+        static std::map<std::string, std::unique_ptr<Material>> materialsPool;
+
+        std::map<std::string, std::unique_ptr<Material>>::iterator myMaterial;
+
         std::unique_ptr<ShaderModule> vert, frag;
         std::unique_ptr<PipelineLayout> pipelineLayout;
         std::unique_ptr<PipelineCache> pipelineCache;
         std::unique_ptr<GraphicsPipeline> graphicsPipeline;
-
+        std::vector<VkDescriptorSet> descriptorSets;
         GraphicsPipelineInfo pipelineStateInfo;
         VkGraphicsPipelineCreateInfo pipelineCreateInfo;
+        std::string modelName;
     };
 
 }
