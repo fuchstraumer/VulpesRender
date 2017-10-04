d9267261f12ca4f17d89d54f104ff6250b7bbf2a
Keegan Jones <kjones@tethers.com>
2017-10-04 10:19:39 -0700

    Move vertex_t implementation to source file: make move operator+ctor noexcept, and add explicitly noexcept copy ctors and operators. Make static binding and attribute description objects const instead of constexpr, and move their definition to a source file to avoid undefined symbols when linking to VulpesRender as a library.

--- include/objects/vertex_t.hpp
+++ include/objects/vertex_t.hpp
@@ -12,39 +12,23 @@ namespace vulpes {
     struct vertex_t {
         vertex_t() = default;
         ~vertex_t() = default;
-        vertex_t(const vertex_t&) = default;
 
-        vertex_t(const glm::vec3& _pos, const glm::vec3& _norm = glm::vec3(0.0f), const glm::vec2 _uv = glm::vec2(0.0f)) :
-            pos(_pos), normal(_norm), uv(_uv) {}
+        vertex_t(const glm::vec3& _pos, const glm::vec3& _norm = glm::vec3(0.0f), const glm::vec2& _uv = glm::vec2(0.0f));
+        vertex_t(const vertex_t& other) noexcept;
+        vertex_t(vertex_t&& other) noexcept;
 
-        vertex_t& operator=(const vertex_t&) = default;
-
-        vertex_t& operator=(vertex_t&& other) {
-            pos = std::move(other.pos);
-            normal = std::move(other.normal);
-            uv = std::move(other.uv);
-            return *this;
-        }
-
-        vertex_t(vertex_t&& other) : pos(other.pos), normal(other.normal), uv(other.uv) {}
+        vertex_t& operator=(const vertex_t&) noexcept;
+        vertex_t& operator=(vertex_t&& other) noexcept;
         
         glm::vec3 pos = glm::vec3(0.0f);
         glm::vec3 normal = glm::vec3(0.0f);
         glm::vec2 uv = glm::vec2(0.0f);
 
-		bool operator==(const vertex_t& other) const noexcept {
-			return (pos == other.pos) && (normal == other.normal) && (uv == other.uv);
-		}
+		bool operator==(const vertex_t& other) const noexcept;
         
-        constexpr static VkVertexInputBindingDescription bindingDescription{
-            0, (sizeof(glm::vec3) * 2) + sizeof(glm::vec2), VK_VERTEX_INPUT_RATE_VERTEX
-        };
+        static const VkVertexInputBindingDescription bindingDescription;
 
-        constexpr static std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{
-            VkVertexInputAttributeDescription{ 0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0 },
-            VkVertexInputAttributeDescription{ 1, 0, VK_FORMAT_R32G32B32_SFLOAT, sizeof(glm::vec3) },
-            VkVertexInputAttributeDescription{ 2, 0, VK_FORMAT_R32G32_SFLOAT, sizeof(glm::vec3) * 2 }
-        };
+        static const std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions;
     };
 }
 
