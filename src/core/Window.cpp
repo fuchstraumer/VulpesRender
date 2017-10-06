a8188b9e81225bc5cba8fa9686ffbc96f95e803f
Keegan <jonesk74@uw.edu>
2017-10-05 18:06:03 -0700

    Added actual usage of fullscreen mode flag from SceneConfig struct.

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -20,8 +20,18 @@ namespace vulpes {
     void Window::createWindow() {
 
         std::string window_title = BaseScene::SceneConfiguration.ApplicationName;
-        window = glfwCreateWindow(static_cast<int>(width), static_cast<int>(height), window_title.c_str(), nullptr, nullptr);
-
+		if (!BaseScene::SceneConfiguration.EnableFullscreen) {
+			window = glfwCreateWindow(static_cast<int>(width), static_cast<int>(height), window_title.c_str(), nullptr, nullptr);
+		}
+		else {
+			GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+			const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+			glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+			glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+			glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+			glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+			window = glfwCreateWindow(mode->width, mode->height, window_title.c_str(), monitor, nullptr);
+		}
         ImGuiIO& io = ImGui::GetIO();
 #ifdef _WIN32
 		io.ImeWindowHandle = glfwGetWin32Window(window);
