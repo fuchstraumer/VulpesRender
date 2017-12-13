f5ebf81c66fb6e8a4c6e1ad0bbdf57f5f0a733c5
Keegan <jonesk74@uw.edu>
2017-12-12 16:02:48 -0800

    Add fence object.

---
+++ include/resource/Fence.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef VPR_FENCE_HPP
+#define VPR_FENCE_HPP
+#include "vpr_stdafx.h"
+#include "ForwardDecl.hpp"
+
+namespace vpr {
+
+    class Fence {
+        Fence(const Fence&) = delete;
+        Fence& operator=(const Fence&) = delete;
+    public:
+        
+    };
+
+}
+#endif //!VPR_FENCE_HPP
